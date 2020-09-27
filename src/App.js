efaca90a51c8869280dfb459e089aad081e95c15
ithaksinterlance <it.haksinterlance@gmail.com>
2020-09-27 18:31:23 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import hakstimeBg from "./img/moto.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hakstime deploy</h2>
+      <img src={hakstimeBg} alt="hakstime-bg" />
+    </div>
+  );
+}
+
+export default App;
