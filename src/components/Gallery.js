10594f9cd325c772f340cbdde9c06e5b8cd767e3
ithaksinterlance <it.haksinterlance@gmail.com>
2020-12-26 12:56:36 +0530

    commit

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -9,7 +9,10 @@ export default class Gallery extends Component {
         <div className="container-fluid">
           <div className="row padding pt-5">
             <div className="col-md-12">
-              <h1>Under construction... We will update soon;)</h1>
+              <h1>
+                Under construction...{" "}
+                <p className="lead">We will update soon;)</p>
+              </h1>
             </div>
           </div>
         </div>
